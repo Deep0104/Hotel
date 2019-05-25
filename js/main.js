6ededc3c5e5cefac97a95d2cdd202f05265806b8
Anastasia <anastasibohdan45@gmail.com>
2019-05-25 09:36:03 +0300

    10

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+//Slider
 function slide(container) {
 	var img = container.getElementsByTagName('img');
 	var count = 0;
@@ -30,8 +32,38 @@ function slide(container) {
 			img[i].style.opacity = '0';
 		}
 		count++;
-	}, 3000)
+	}, 3500)
 }
 var slider = document.getElementById('slider');
-
 slide(slider);
+
+//Search
+// function Search(){
+// 	var text = document.getElementById('search').value
+// 	var search = new RegExp(text, "ig");
+// 	var body = document.getElementsByTagName('body')[0];
+// 	for (var i = 0; i < body.childNodes.length; i++) {
+// 		var content = body.childNodes;
+// 		var textt = content.innerHTML;
+// 		if (textt.search(search) != -1) {
+//     		content[i].setAttribute('style', 'background-color:yellow');
+//     		}
+// 	}
+// }
+function searchTextOnPage(inputId) { 
+  var obj = window.document.getElementById(inputId); 
+  var textToFind; 
+  if (obj) { 
+    textToFind = obj.value; 
+  } else { 
+    alert("Немогу найти текст = " + inputId); 
+    return; 
+  } 
+  if (textToFind == "") { 
+    alert("Вы ничего не ввели"); 
+    return; 
+  } 
+  document.body.innerHTML = document.body.innerHTML.replace(eval("/"+textToFind+"/g","<b style='color:red'>"+textToFind+"</b>")); 
+  var obj = window.document.getElementById(inputId); 
+  obj.value = textToFind; 
+} 
